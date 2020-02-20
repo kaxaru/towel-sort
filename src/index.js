ad620c6ebc382bc596776e77e3eaf8e13aca93e5
Kaxaru <khidechi@gmail.com>
2020-02-20 12:07:06 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+  flattenDeep = (arr1) => arr1.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), [])
+  return flattenDeep(matrix.map((arr, idx) => idx%2 != 0 ? arr.reverse() : arr)) 
 }
